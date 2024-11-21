6b8cd6d56e2a1124c87ce6455dbc59719e3b6209
Jonathan Kitt <jonathan.kitt@inrae.fr>
2024-11-21 17:21:25 +0100

    update

---
+++ 00-1-age-and-year-of-deathofharold-shipmans-victims/00-1-age-year-shipman victims-x.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+df <- read_csv("00-1-age-and-year-of-deathofharold-shipmans-victims/00-1-shipman-confirmed-victims-x.csv") 
+
+head(df)
+
+ggplot(data = df,
+       aes(x = fractionalDeathYear, y = Age,
+           col = reorder(gender2, gender))) +
+  geom_point(size = 2) +
+  labs(x = "Year", y = "Age of victim") +
+  scale_x_continuous(breaks = seq(1975, 1995, 5),
+                     limits = c(1974, 1998)) +
+  scale_y_continuous(breaks = seq(40, 90, 10),
+                     limits = c(39, 95)) +
+  scale_color_brewer(name = "Set1") +
+  theme(legend.position.inside = c(0.125, 1.12),
+        legend.background = element_rect(colour = "black"),
+        legend.title = element_blank())
